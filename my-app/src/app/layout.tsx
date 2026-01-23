59b5adfafeda6504ea6af7efd44a9786a9c44936
Abdulqudus <abdullateefqudusleeq@gmail.com>
2026-01-23 13:12:36 +0100

    Update layout.tsx

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { useEffect } from 'react'
 import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = {
